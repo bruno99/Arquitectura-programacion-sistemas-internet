dac8853b5a4c8e7343ffb9dff08b99c33363d6a9
Bruno Urbán <43136694+bruno99@users.noreply.github.com>
2020-09-17 14:40:48 +0200

    Create JavaScript-Fundamentos.js

---
+++ JavaScript-Fundamentos.js
@@ -0,0 +1,27 @@
+VSCODE 
+Definir variables. 3 formas:
+ 1. 
+ var a = 3; 
+ OBSOLETO NO USAR
+ 2.
+ let a = 5;
+ 3.Cuando no se vaya a reasignar, es decir, que no vaya a darle otro valor en ningún lugar del código. SIEMPRE USAR CONST SALVO CUANDO NO SE PUEDA
+ const a = 5;
+ Tampoco se podría hacer a++ porque sería reasignar como a = a+1.
+ Lo que si se podría sería tener const a= [1,2,3] y hacer a.push(5), que sería a=[1,2,3,5]
+ Para comparar a===b significa que apunten a lo mismo, no que sean el mismo valor(lo que seria a==b). 
+ 
+ RECUERDA-> EN TYPESCRIPT SE TIPA:
+ number
+ string
+ boolean
+ any
+ undefined
+ 
+ ¿para qué se usa undefined? Cuando no sé si se me a va a devolver el tipo que estoy pidiendo. Ej/ let image: Image|undefined = getImage(); por si la imagen no existe
+ La funcion typeof me dice el tipo. Ej/ if(typeof(fecha) == "string") haría algo si fecha es un tipo string
+ El for se declara igual que el c++.
+ El string se puede definir :
+ -let a:string= "hola"; //usar esta seguramente
+ -let a:string= 'hola';
+ -let a:string= '''hola'''; -> "ho/nla"
