aaf6e181b7b0d0093e8c749fa7124d20c7a024ea
Bruno Urbán <43136694+bruno99@users.noreply.github.com>
2020-10-01 15:05:27 +0200

    Update Ej1.js

--- Ejercicios-Entregables/Ej1.js
+++ Ejercicios-Entregables/Ej1.js
@@ -1,1 +1,7 @@
 
+Recibira 2 parametros y tiene que ir recibiendo cada uno de sus elementos y compararlos. Si es un basico lo compara y ya, si es un array entrara a cada uno de sus elementos.
+Importante e object keys, saber si es array y objeto pra entrar elemento a elemento
+Hacer una funcion que sea print obect pero que imprima en profundidad, y luego el de comparacion de 2 y finalmente el clonado (construir otro igual donde copie los valores, 
+no la referecnia)
+clone deep y equal deep. Recursividad
+Primero comparar la referecnia === porque si la referencia es la misma no tengo por que entrar 
