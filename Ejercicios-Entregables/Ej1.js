c2ee5879cc919e34417dabb2e0c172c2262bfb5b
Bruno Urbán <43136694+bruno99@users.noreply.github.com>
2020-10-02 12:02:11 +0200

    Update Ej1.js

--- Ejercicios-Entregables/Ej1.js
+++ Ejercicios-Entregables/Ej1.js
@@ -1,4 +1,6 @@
 
+*****SIN ACABAR****NO CORREGIR AUN*****
+
 Recibira 2 parametros y tiene que ir recibiendo cada uno de sus elementos y compararlos. Si es un basico lo compara y ya, si es un array entrara a cada uno de sus elementos.
 Importante e object keys, saber si es array y objeto pra entrar elemento a elemento
 Hacer una funcion que sea print obect pero que imprima en profundidad, y luego el de comparacion de 2 y finalmente el clonado (construir otro igual donde copie los valores, 
