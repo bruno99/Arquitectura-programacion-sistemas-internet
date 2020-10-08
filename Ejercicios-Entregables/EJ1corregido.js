35edcd934d492752cb6687f8ad72b10d8b719ac5
Bruno Urbán <43136694+bruno99@users.noreply.github.com>
2020-10-08 13:14:08 +0200

    Create EJ1corregido.js

---
+++ Ejercicios-Entregables/EJ1corregido.js
@@ -0,0 +1,18 @@
+//print
+const obj 0 {
+   name: "alberto",
+   edad: 20
+   amigos: [
+       "Juan, "Maria", "Paula"
+       ],
+   mascotas: [
+       {
+            name: "Pipo",
+            raza: "perro"
+       },
+       {
+            name: "lily",
+            raza: "gato",
+       },
+     ]
+  }   
