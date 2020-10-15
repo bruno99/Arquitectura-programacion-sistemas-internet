2b8865db5c207414c5697873565e2ffa829386f3
Bruno Urbán <43136694+bruno99@users.noreply.github.com>
2020-10-15 13:11:58 +0200

    Update RickMortyApi.ts

--- RickMortyApi.ts
+++ RickMortyApi.ts
@@ -10,14 +10,38 @@ const jsonData = await response.json();
 console.log(jsonData);
 */
 //sacar todos los personajes que se llamen Rick
+
 const json = fetch("https://rickandmortyapi.com/api/character/?name=rick");
 json.then((response)=> {
   return response.json();
 }).then((jsonData) => {
   console.log(jsonData);
 });
+
 //Cual es el personaje que aparece en mas episodios?
-if(!obj[nombre])
-    obj[nombre]=0;
-else{
-     obj[nombre]++;
+interface IInfo{
+  next: string;
+}
+interface IResult{
+  characters: string[];
+}
+interface IData {
+  info: Iinfo;
+  results: IResult[];
+}
+const url = "https://rickandmortyapi.com/api/episode?=1";
+ 
+let response = await Fetch(url);
+const data: IData = await response.json();
+
+while(data.info.next){
+  response = await fetch(data.info.next);
+  data = await response.json;
+}
+
+console.log("done");
+
+
+
+
+
